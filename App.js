3dc140464a8544acb1f7620653070597b6508825
harshithabhanu <129141932+harshithabhanu@users.noreply.github.com>
2023-12-08 16:16:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Transaction/todo";
+import React from "react";
+
+export default function App(){
+  // Create state for the expenses here
+
+  
+    return (
+      <>
+       <Todo/>
+      </>
+    );
+  }
+
